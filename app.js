fd56c0da5bd2005b122d225841d29efaf3aabff8
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-06 21:19:51 -0500

    init commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const uri = 'mongodb://localhost:27017';
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
+
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
@@ -15,7 +16,33 @@ async function run() {
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
+  }  
+}
+
+
+
+
+async function run() {
+  try {
+    const database = client.db("fruitsDB");
+    const fruits = database.collection("fruits");
+    // create an array of documents to insert
+    const docs = [
+      { name: "Apple", score: 8, review:"Great fruit" },
+      { name: "Orange", score: 6, review:"Kinda sour" },
+      { name: "Banana", score: 9, review: "Great stuff!" }
+    ];
+    // this option prevents additional documents from being inserted if one fails
+    const options = { ordered: true };
+    const result = await fruits.insertMany(docs, options);
+    console.log(`${result.insertedCount} documents were inserted`);
+  } finally {
+    await client.close();
   }
 }
+
+
+
+
 run().catch(console.dir);
 
